f5ac38032f80597d21c5a5187e52c6fe48bc9724
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-06-29 15:02:08 +0200

    create scroll effect

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -3,6 +3,7 @@ const navSection = document.querySelector(".navSection");
 const navSectionLinks = document.querySelectorAll(".navSection__left--nav a")
 const menuOpenIcon = document.querySelector(".menu-open-icon")
 const menuCloseIcon = document.querySelector(".menu-close-icon")
+const moreAboutMe = document.querySelector(".moreAboutMe")
 
 menuCloseIcon.classList.add("hide")
 navSectionLinks.forEach(el=>{
@@ -10,19 +11,8 @@ navSectionLinks.forEach(el=>{
 })
 
 const openBurgerMenu = e =>{
-    navSectionLinks.forEach(el=>{
-        if(el===e.target){
-            el.style.color = "red"
-            el.style.filter = "blur(0px)"
-        }
-        else{
-            el.style.color = "black"
-            el.style.filter = "blur(1.5px)"
-        }
-    })
     if(e.target.matches(".menu-open-icon")){
         navSectionLinks.forEach(el=>{
-            el.style.filter = "blur(0px)"
             el.classList.remove("hideMenu")
         })
         menuOpenIcon.classList.add("hide")
@@ -37,6 +27,28 @@ const openBurgerMenu = e =>{
     }
 }
 
+const navEffect = e =>{
+    navSectionLinks.forEach(el=>{
+        el.style.filter = "blur(0px)"
+        if(el!==e.target){
+            el.style.color = "black"
+            el.style.filter = "blur(1.5px)"
+        }
+        else{
+            el.style.color = "red"
+            el.style.filter = "blur(0px)"
+        }
+    })
+}
+
+const removeNavEffect = () =>{
+    navSectionLinks.forEach(el=>{
+        el.style.filter = "blur(0px)"
+        el.style.color = "black"
+    })
+}
 
 
 navSection.addEventListener("click", openBurgerMenu)
+navSection.addEventListener("mouseover", navEffect)
+navSection.addEventListener("mouseout", removeNavEffect)
